769ac57f14ed0a4dee87cef16a8e8894d3d372f6
Reid Mitchell <reidmitchell1@gmail.com>
2018-03-16 22:22:34 -0700

    One-line an if statement

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -4,9 +4,7 @@ const ReactDOM = require('react-dom');
 
 beforeEach(() => {
   const root = document.getElementById('root');
-  if (root) {
-    document.body.removeChild(root);
-  }
+  if (root) document.body.removeChild(root);
   const newRoot = document.createElement('div');
   newRoot.setAttribute('id', 'root');
   document.body.appendChild(newRoot);
